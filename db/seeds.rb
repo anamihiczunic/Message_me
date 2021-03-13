064a173ae4d858b462367aefd9e3a59714dec062
Ana Mihic Zunic <anamihiczunic@gmail.com>
2021-03-13 17:15:18 +0100

    Add users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Ana", password: "banana")
+User.create(username: "Kiki", password: "bankiki")
